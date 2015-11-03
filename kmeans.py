f9a5f3ad500ba126eb78f3f20d194b3193cac2dc
heshenghuan <heshenghuan@sina.com>
2015-11-03 15:01:30 +0800

    added kmeans.py

---
+++ kmeans.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 14:52:39 2015-11-03
+
+@author: heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+import codecs
+
+class kmeans:
+    def __init__(self):
+        self.clusters = None
+        self.samples = None
